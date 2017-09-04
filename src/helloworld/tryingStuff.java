1036dcba8b93d820be12001759dfe266604e4f66
Karl Kadalipp <karl.kadalipp@finestmedia.ee>
2017-09-04 18:02:40 +0300

    PROOV-1 - something

--- src/helloworld/tryingStuff.java
+++ src/helloworld/tryingStuff.java
@@ -61,6 +61,8 @@ public class tryingStuff {
 //		
 //		String f = String.valueOf(Float.parseFloat(String.valueOf((Float.floatToRawIntBits((float)new Long(new Integer(1)))))));
 //		System.out.println("f = " + f);
+		
+		proov1();
 	}
 	
 	
@@ -71,5 +73,9 @@ public class tryingStuff {
 	
 	private static String fixTypoRemore(String serviceName){
 		return serviceName.replaceAll("remore", "remote");
+	}
+	
+	private static void proov1(){
+		System.out.println("proov1");
 	}
 }
